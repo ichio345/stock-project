33edf0fe4df86ea0048557d92d47f70ef6f7fbd6
ichio345 <MAC@JasonJason.local>
2016-11-29 16:23:32 +0800

    11/29

---
+++ 11:28.js
@@ -0,0 +1,31 @@
+
+	function{
+			var target = document.getElementById["select1"];
+			alert(target);
+	}
+	var beginYear
+	var Endyear
+	var value = 10
+	var comparison = ">"
+	var target = "stocks"
+	var csv = ".csv"
+	var dataPath = "https://ichio345.github.io/stock-project/"
+	var dataURL = dataPath+target+csv
+	var x
+d3.csv(dataURL, function search(){
+	var returnTarget = target
+	return{
+		'company':d.name
+		'data':+d.returnTarget
+	}
+	if (data-value<0){
+	x=0
+}else{
+	x=1
+}
+Boolean(x)
+
+
+})
+
+
