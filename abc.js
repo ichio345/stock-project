f156203d1c4791c45665620b4a1c7629cb723a14
ichio345 <MAC@ProjectD.m7.ntu.edu.tw>
2016-12-20 17:39:22 +0800

    oo

--- abc.js
+++ abc.js
@@ -190,7 +190,7 @@ for(m=0;m<outcome.length;m++){
  var y = outcome[m]
  var newDiv = document.createElement("button"); 
  newDiv.setAttribute("id", y["id"]) ;
- newDiv.setAttribute("onclick", al())
+ newDiv.setAttribute("onclick", "al()")
  newDiv.style.backgroundColor = "Gainsboro";
  newDiv.style. width = "100%"
  newDiv.style.textAlign = "center"
