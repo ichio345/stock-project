f0ab2330af77aa7cf99a846d7cb1f7a4b4ff0260
ichio345 <MAC@ProjectD.m7.ntu.edu.tw>
2016-12-25 02:14:50 +0800

    s

--- abc.js
+++ abc.js
@@ -161,7 +161,7 @@ for(m=0;m<outcome.length;m++){
  var newDiv = document.createElement("button"); 
  newDiv.setAttribute("id", y["id"]) ;
  var z = document.getElementById(y["id"]);
-z.onclick = function(){alert("hi")};
+ alert(y["id"]);
  newDiv.style.backgroundColor = "Gainsboro";
  newDiv.style. width = "100%"
  newDiv.style.textAlign = "center"
