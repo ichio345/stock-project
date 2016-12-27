aa8d36daf417c61f67612323d43de1411578e235
ichio345 <MAC@JasonJason.local>
2016-12-27 13:16:05 +0800

    s

--- abc.js
+++ abc.js
@@ -8,7 +8,7 @@
     var text = 10
     var testrequest = {target:"NATPM", comparison:1, data :"2", boolean:1}
     var testrequest2 = {target:"ROE", comparison:2, data:"3", boolean:1}
-    var testrequest3 = {target:"IT", comparison:0, data:"20", boolean:1}
+    var testrequest3 = {target:"IT", comparison:1, data:"20", boolean:1}
     var testrequest4 = {target:"0", comparison:0,data:"3", boolean:0}
     
      var result1 = []
